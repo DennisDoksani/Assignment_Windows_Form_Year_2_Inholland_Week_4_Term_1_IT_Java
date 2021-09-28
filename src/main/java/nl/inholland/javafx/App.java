8233beed1452deac653035771b5115e325176abf
DennisDoksani <70758915+DennisDoksani@users.noreply.github.com>
2021-09-28 17:07:31 +0200

    finished hw

--- src/main/java/nl/inholland/javafx/App.java
+++ src/main/java/nl/inholland/javafx/App.java
@@ -6,6 +6,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -14,6 +15,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
+import static java.awt.Color.*;
 
 
 public class App extends Application {
@@ -23,8 +27,11 @@ public class App extends Application {
         window.setWidth(800);
         window.setTitle("In holland JavaFX Starter Project");
 
-
         GridPane gridPane = new GridPane();
+
+        gridPane.setPadding(new Insets(10,10,10,10));
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
 
         Label usernameLabel = new Label("Username:");
         gridPane.setConstraints(usernameLabel,0,0);
@@ -43,10 +50,13 @@ public class App extends Application {
         gridPane.setConstraints(passwordField,1,1);
 
         Button loginButton = new Button("Log in");
-        GridPane.setConstraints(loginButton,1,2);
+        GridPane.setConstraints(loginButton,1,3);
 
         Label secretLabel = new Label();
         GridPane.setConstraints(secretLabel,0,3);
+
+        Label errorLabel = new Label();
+        GridPane.setConstraints(errorLabel,1,2);
 
 
         gridPane.getChildren().add(usernameLabel);
@@ -55,17 +65,53 @@ public class App extends Application {
         gridPane.getChildren().add(passwordField);
         gridPane.getChildren().add(loginButton);
         gridPane.getChildren().add(secretLabel);
+        gridPane.getChildren().add(errorLabel);
 
         //create a StringProperty
         StringProperty passwordProperty = passwordField.textProperty();
-
+        loginButton.setVisible(false);
         //add the listener to this property
         passwordProperty.addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue,
                                 String oldValue, String newValue)
             {
-                loginButton.setVisible(secretLabel.getText().length() != 0);
+                //errorLabel.setTextFill(red);  <--- Don't know why when I try to change the color there is an error popping up? :/
+                String error = errorLabel.getText();
+                String password = passwordField.getText();
+
+
+                boolean validChars = password.matches(".*[a-zA-Z]+.*");
+                boolean validNums = password.matches(".*[0-9]+.*");
+                boolean validSymbol = password.matches(".*[!@#$%^&*()-+]");
+                loginButton.setVisible(false);
+                if(password.length() <= 8)
+                {
+                    error = "Please enter more than 8 characters";
+                    errorLabel.setText(error);
+                }
+                else if(validChars == false && validNums && validSymbol)
+                {
+                    error = "You are missing a lower or an upper case letter";
+                    errorLabel.setText(error);
+                }
+                else if (validSymbol == false && validChars && validNums)
+                {
+                    error = "You are missing a special character";
+                    errorLabel.setText(error);
+                }
+                else if(validNums == false && validChars && validSymbol)
+                {
+                    error = "Your password must require at least a number character";
+                    errorLabel.setText(error);
+                }
+                else if (validChars && validNums && validSymbol && password.length() >= 8)
+                {
+                    error = "";
+                    errorLabel.setText(error);
+                    loginButton.setVisible(true);
+                }
+
             }
         });
         //bind the listener to the text property of the new label
@@ -74,6 +120,7 @@ public class App extends Application {
         loginButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
+
                 System.out.println(usernameInput.getText());
             }
         });
